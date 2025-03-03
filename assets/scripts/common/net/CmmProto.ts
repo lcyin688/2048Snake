72addba39337744027f9bb368a5dcdf3d8c62874
lcyin688 <254085850@qq.com>
2025-03-03 20:57:40 +0800

    清空

--- assets/scripts/common/net/CmmProto.ts
+++
@@ -1,7 +0,0 @@
-import { ProtoMessage } from "../../framework/core/net/message/ProtoMessage";
-import {QLMessageStruct} from "../../framework/core/net/message/SSMCodec";
-
-/**@description 根据自己项目扩展 */
-export class CmmProto<T> extends ProtoMessage<T> implements QLMessageStruct{
-    cmd : string | number = "";
-}
